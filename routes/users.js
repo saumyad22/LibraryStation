35fb7c5341838740627b41ad0957e70dc0304898
Saumya <kdevara.saumya@gmail.com>
2017-09-06 22:50:27 -0700

    adding files to new repo

---
+++ routes/users.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var userController = require('../controller/users.js');
+var authController = require('../controller/auth');
+
+console.log("heelo hoo r ju in routes")
+router.route('/')
+  .post(userController.postUsers)
+  .get(authController.isAuthenticated, userController.getUsers);
+
+router.route('/:username')
+  .get(authController.isAuthenticated, userController.getUser)
+  .put(authController.isAuthenticated, userController.putUser)
+  .delete(authController.isAuthenticated, userController.deleteUser);
+
+module.exports = router;
